f46a7cbcd6b8f539c79db2fb784cf831dbdbd8aa
William Antoniazzi <62269345+williamantoniazzi@users.noreply.github.com>
2023-04-11 20:23:22 -0300

    inicio labdev

---
+++ src/main/java/fatec/labbd/provalabbd/ProvalabbdApplication.java
@@ -0,0 +1,13 @@
+package fatec.labbd.provalabbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvalabbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvalabbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fatec/labbd/provalabbd/ProvalabbdApplicationTests.java
@@ -0,0 +1,13 @@
+package fatec.labbd.provalabbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvalabbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
