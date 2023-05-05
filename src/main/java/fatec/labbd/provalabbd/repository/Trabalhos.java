24d54ba56442196cbaff265e54528dc4e5929575
William Antoniazzi <william.antoniazzi@fatec.sp.gov.br>
2023-05-05 03:20:40 +0000

    Correção e programa rodando.

--- src/main/java/fatec/labbd/provalabbd/repository/Trabalhos.java
+++ src/main/java/fatec/labbd/provalabbd/repository/Trabalhos.java
@@ -1,8 +1,6 @@
 package fatec.labbd.provalabbd.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import fatec.labbd.provalabbd.entity.Trabalho;
 
 public interface Trabalhos extends JpaRepository<Trabalho, Long>{
